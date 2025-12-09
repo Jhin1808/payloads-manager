5bbd5e6e531fbb18548cd415aa21db1e2b1423b0
Luan Nguyen <luan1808@uw.edu>
2025-12-09 01:34:42 -0800

    url pattern for view

--- payloads/urls.py
+++ payloads/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("parts/", views.part_list, name="parts_list")
+    path("parts/", views.parts_list, name="parts_list")
 ]
